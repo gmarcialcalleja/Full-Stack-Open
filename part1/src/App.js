7394a53739df906fbe19107fc40b3db604d2aa86
gmarc <gmarcialcalleja@gmail.com>
2021-09-19 22:07:15 -0400

    added button component, Separate experiment

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,44 +1,80 @@
 import React, { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
+const History = (props) => {
+  if (props.allClicks.length === 0 ) {
+    return (
+      <div>Press the damn button idiot</div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
-
-const Buttons = ({ onClick,text }) => <button onClick={ onClick }> { text }</button>
-
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClicks}>
+      {props.text}
+    </button>
+  )
+}
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([]);
 
-  
-  const timeErase = () => setCounter(counter+ 1);
-  const bitesTheDust = () => setCounter(counter - 1);
-  const returnToZero = () =>  setCounter(0);
+  const handleClicks = {
+    plus: {
+      left: () => {
+        setAll(allClicks.concat('L'));
+        setLeft(left + 1);
+      },
+      right: () => {
+        setAll(allClicks.concat('R'));
+        setRight(right + 1);
+      }
+    },
 
+    minus: {
+      left: () => {
+        setAll(allClicks.concat('-L'));
+        setLeft(left - 1);
+      },
+      right: () => {
+        setAll(allClicks.concat('-R'));
+        setRight(right - 1);
+      }
+    }
+
+  }/*
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  }*/
 
   return (
-    <>
-      <Display counter={counter}/>
-      <Buttons
-    
-      onClick={timeErase}
-      text='Plus'
-
+    <div>
+      {left}{/*
+      <button onClick={handleClicks.plus.left}>left</button>
+      <button onClick={handleClicks.plus.right}>right</button>*/}
+      <Button 
+      handleClicks={handleClicks.plus.left}
+      text="left"
       />
-
-      <Buttons
-
-      onClick={returnToZero}
-      text='RTZ'
-
+      <Button
+      handleClicks={handleClicks.plus.right}
+      text="right"
       />
-
-      <Buttons 
-      
-      onClick={bitesTheDust}
-      text="Minus"
-      
-      />
-    </>
+      {right}
+      <History allClicks={allClicks}/>
+    </div>
   )
 }
 
