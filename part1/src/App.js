34587acc816ef3e683975cb6373dfc980dce3185
gmarc <gmarcialcalleja@gmail.com>
2021-09-19 20:19:34 -0400

    react Countdown states

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,71 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.courseName}</h1>
-    </>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Parts = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </>
-  )
-}
 
-const Content = (props) => {
+const Buttons = ({ onClick,text }) => <button onClick={ onClick }> { text }</button>
 
-  console.log(props);
 
-  return (
-    <>
-      <Parts part={props.part1} exercises={props.exercises1} />
-      <Parts part={props.part2} exercises={props.exercises2} />
-      <Parts part={props.part3} exercises={props.exercises3} />
-    </>
-  )
-}
-
-const Total = (props) => {
-
-  console.log(props);
-
-  return (
-    <>
-      {props.exercises1 + props.exercises2 + props.exercises3}
-    </>
-  )
-}
- 
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
 
   
-  const course = 'Half Stack Application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  
+  const timeErase = () => setCounter(counter+ 1);
+  const bitesTheDust = () => setCounter(counter - 1);
+  const returnToZero = () =>  setCounter(0);
+
+
   return (
     <>
-      <Header courseName={course}/>
-      <Content part1={parts[0].name} part2={parts[1].name} part3={parts[2].name} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Display counter={counter}/>
+      <Buttons
+    
+      onClick={timeErase}
+      text='Plus'
+
+      />
+
+      <Buttons
+
+      onClick={returnToZero}
+      text='RTZ'
+
+      />
+
+      <Buttons 
+      
+      onClick={bitesTheDust}
+      text="Minus"
+      
+      />
     </>
   )
 }
